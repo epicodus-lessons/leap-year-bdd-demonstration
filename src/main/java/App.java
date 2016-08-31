f052622718962684363c49134d8b6bbee456ec70
Courtney Phillips <courtney@epicodus.com>
2016-08-31 16:58:43 -0700

    Add prompt to command line interface requesting users enter a year.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,7 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
+
+    System.out.println("Enter a year, we'll tell you if it's a leap year:")
   }
 }
