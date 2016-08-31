a6eea0da01e5e69aec16c34ec00a952606d591af
Courtney Phillips <courtney@epicodus.com>
2016-08-31 16:24:47 -0700

    Add logic to return true when years are divisible by four.

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,8 +1,7 @@
 public class LeapYear {
-  public static void main(String[] args) {}
 
   public boolean isLeapYear(Integer year) {
-    // eventually put your code here
-    return false;
+    return year % 4 == 0;
   }
+  
 }
