5f030d9eaa5b4d90032d79cea4728ff9db6b06ed
Courtney Phillips <courtney@epicodus.com>
2016-08-31 17:02:26 -0700

    Fix typo in App.java.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
-    System.out.println("Enter a year, we'll tell you if it's a leap year:")
+    System.out.println("Enter a year, we'll tell you if it's a leap year:");
     String stringYear = myConsole.readLine();
     int intYear = Integer.parseInt(stringYear);
     boolean leapYearResult = isLeapYear(intYear);
