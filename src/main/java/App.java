f48380697dc166ebb1aed488119ce25b12665587
Courtney Phillips <courtney@epicodus.com>
2016-08-31 17:00:22 -0700

    Retrieve and parse user input.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,8 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
-
     System.out.println("Enter a year, we'll tell you if it's a leap year:")
+    String stringYear = myConsole.readLine();
+    int intYear = Integer.parseInt(stringYear);
   }
 }
